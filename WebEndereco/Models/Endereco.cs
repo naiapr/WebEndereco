f2add7bc2a0bf7627efe36fef8a472bec83e5a8d
naiapr <naiapr@gmail.com>
2019-07-06 15:51:48 -0300

    alt

--- WebEndereco/Models/Endereco.cs
+++ WebEndereco/Models/Endereco.cs
@@ -12,16 +12,11 @@ namespace WebEndereco.Models
         public class Endereco
         {
             [Key]
-        public int Id { get; set; }
-        public string Cep { get; set; }
-        public string Logradouro { get; set; }
+        public int EnderecoID { get; set; }
         public string Complemento { get; set; }
         public string Bairro { get; set; }
-        public string Localidade { get; set; }
-        public string Uf { get; set; }
-        public string Unidade { get; set; }
-        public string Ibge { get; set; }
-        public string Gia { get; set; }
+        
+      
     }
     
 }
